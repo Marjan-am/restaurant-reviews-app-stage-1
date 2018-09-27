afb191e17f66b84efe4078168bc82722f3f7b3f7
marjan <marjan.arod@gmail.com>
2018-09-27 15:43:28 -0700

    Adding service worker

---
+++ js/sw_register.js
@@ -0,0 +1,13 @@
+  /**
+   *     Service Worker registration
+   */
+  
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./sw.js')
+	.then(function() {
+		console.log('Registration worked!');
+	})
+	.catch(function() {
+		console.log('Registration failed!');
+	});
+}
